e1e7ddd9739ed9f48146d7e1446d49dbba81b830
yunpengxiao <yunpeng.shaw@gmail.com>
2025-02-26 12:43:41 -0800

    exercises

--- exercises/14_generics/generics1.rs
+++ exercises/14_generics/generics1.rs
@@ -6,9 +6,10 @@ fn main() {
     // TODO: Fix the compiler error by annotating the type of the vector
     // `Vec<T>`. Choose `T` as some integer type that can be created from
     // `u8` and `i8`.
-    let mut numbers = Vec::new();
+    let mut numbers: Vec<i32> = Vec::new();
 
     // Don't change the lines below.
+    // Compiler will infer what type is needed here and call the corresponding into().
     let n1: u8 = 42;
     numbers.push(n1.into());
     let n2: i8 = -1;
